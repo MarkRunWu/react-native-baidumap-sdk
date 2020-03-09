dab94777b57539c0636cb811d94c1cb996b8b817
mark <saybyebyet@gmail.com>
2020-03-09 19:11:00 +0800

    Update js code

--- lib/js/index.d.ts
+++ lib/js/index.d.ts
@@ -1,234 +1,45 @@
-import { Component, ReactElement } from 'react';
-import { ViewProps, ViewStyle } from 'react-native';
-
-export type Point = {
-  x: number;
-  y: number;
+import MapView from "./map-view";
+import Location from "./modules/location";
+import Geocode from "./modules/geocode";
+import Initializer from "./modules/initializer";
+declare const _default: {
+    MapView: typeof MapView;
+    Location: {
+        init(): Promise<void>;
+        start: () => any;
+        stop: () => any;
+        setOptions: (options: {
+            gps: boolean;
+            distanceFilter: number;
+        }) => any;
+        addLocationListener: (listener: (listener: {
+            timestamp: number;
+            altitude: number;
+            speed: number;
+        } & import("./types").Location) => void) => any;
+    };
+    Geocode: {
+        search(address: string, city?: string): Promise<{
+            address: string;
+        } & import("./types").LatLng>;
+        reverse(coordinate: import("./types").LatLng): Promise<{
+            country: string;
+            countryCode: string;
+            province: string;
+            city: string;
+            cityCode: string;
+            district: string;
+            street: string;
+            streetNumber: string;
+            businessCircle: string;
+            adCode: string;
+            address: string;
+            description: string;
+        } & import("./types").LatLng>;
+    };
+    Initializer: {
+        init(key: string): any;
+    };
 };
-
-export type LatLng = {
-  latitude: number;
-  longitude: number;
-};
-
-export type Region = {
-  latitudeDelta: number;
-  longitudeDelta: number;
-} & LatLng;
-
-export type MapStatus = {
-  center: LatLng;
-  region: Region;
-  overlook: number;
-  rotation: number;
-  zoomLevel: number;
-};
-
-export type MapViewStatus = {
-  center?: LatLng;
-  point?: Point;
-  region?: Region;
-  overlook?: number;
-  rotation?: number;
-  zoomLevel?: number;
-};
-
-export type Location = {
-  accuracy?: number;
-  latitude: number;
-  longitude: number;
-  direction: number;
-};
-
-export type EdgePadding = {
-  top: Number;
-  right: Number;
-  bottom: Number;
-  left: Number;
-};
-
-export interface MarkerProps extends ViewProps {
-  coordinate: LatLng;
-  color?: string;
-  image?: string;
-  view?: () => ReactElement<any>;
-  title?: string;
-  selected?: boolean;
-  draggable?: boolean;
-  flat?: boolean;
-  centerOffset?: Point;
-  anchor?: Point;
-  onPress?: () => void;
-  onCalloutPress?: () => void;
-  onDrag?: (coordinate: LatLng) => void;
-  onDragStart?: (coordinate: LatLng) => void;
-  onDragEnd?: (coordinate: LatLng) => void;
-}
-
-export interface CallOutProps extends ViewProps {
-  onPress?: () => void;
-}
-
-export class Callout extends Component<CallOutProps> {}
-
-export class Marker extends Component<MarkerProps> {
-  select(): void;
-  update(): void;
-}
-
-export interface PolylineProps extends ViewProps {
-  points: LatLng[];
-  color?: string;
-  colors?: string;
-  width?: number;
-}
-
-export class Polyline extends Component<PolylineProps> {}
-
-export interface PolygonProps extends ViewProps {
-  points: LatLng[];
-  strokeWidth?: number;
-  strokeColor?: string;
-  fillColor?: string;
-}
-
-export class Polygon extends Component<PolygonProps> {}
-
-export interface CircleProps extends ViewProps {
-  center: LatLng;
-  radius: number;
-  strokeWidth?: number;
-  strokeColor?: string;
-  fillColor?: string;
-}
-
-export class Circle extends Component<CircleProps> {}
-
-export type HeatMapPoint = { intensity: number } & LatLng;
-
-export interface HeatMapProps extends ViewProps {
-  points: HeatMapPoint[];
-  radius?: number;
-  opacity?: number;
-}
-
-export class HeatMap extends Component<HeatMapProps> {}
-
-export type ClusterParams = {
-  id: number;
-  count: number;
-  coordinate: LatLng;
-};
-
-export type ClusterMarkerItem = {
-  coordinate: LatLng;
-  extra?: any;
-};
-
-export interface ClusterProps extends ViewProps {
-  markers: ClusterMarkerItem[];
-  renderMarker: (ClusterMarkerItem: any) => ReactElement<any>;
-  radius?: number;
-  clusterStyle?: ViewStyle;
-  clusterTextStyle?: ViewStyle;
-  renderCluster?: (ClusterParams: any) => ReactElement<any>;
-  onPress?: (ClusterParams: any) => void;
-}
-
-export class Cluster extends Component<ClusterProps> {
-  update(args: { zoomLevel: number; region: Region }): void;
-}
-
-export interface MapViewProps extends ViewProps {
-  satellite?: boolean;
-  trafficEnabled?: boolean;
-  baiduHeatMapEnabled?: boolean;
-  indoorEnabled?: boolean;
-  buildingsDisabled?: boolean;
-  minZoomLevel?: number;
-  maxZoomLevel?: number;
-  compassDisabled?: boolean;
-  zoomControlsDisabled?: boolean;
-  scaleBarDisabled?: boolean;
-  scrollDisabled?: boolean;
-  overlookDisabled?: boolean;
-  rotateDisabled?: boolean;
-  zoomDisalbed?: boolean;
-  center?: LatLng;
-  zoomLevel?: number;
-  rotation?: number;
-  overlook?: number;
-  paused?: boolean;
-  locationEnabled?: boolean;
-  location?: Location;
-  locationMode?: 'normal' | 'follow' | 'compass';
-  campassMode?: true;
-  mapPadding?: EdgePadding;
-  onLoad?: () => void;
-  onClick?: (latlng: LatLng) => void;
-  onLongClick?: (latlng: LatLng) => void;
-  onDoubleClick?: (latlng: LatLng) => void;
-  onStatusChange?: (mapStatus: MapStatus) => void;
-  onDrag?: (coordinate: LatLng) => void;
-  onMove?: () => void;
-}
-
-export class MapView extends Component<MapViewProps> {
-  static Marker: typeof Marker;
-  static Polyline: typeof Polyline;
-  static Polygon: typeof Polygon;
-  static HeatMap: typeof HeatMap;
-  static Cluster: typeof Cluster;
-  static Callout: typeof Callout;
-  static Circle: typeof Circle;
-  setStatus(status: MapViewStatus, duration?: number): void;
-  pointForCoordinate(coordinate: LatLng): Promise<Point>;
-  coordinateForPoint(point: Point): Promise<LatLng>;
-}
-
-export type GeocodeSearchResult = { address: string } & LatLng;
-
-export type GeocodeReverseResult = {
-  country: string;
-  countryCode: string;
-  province: string;
-  city: string;
-  cityCode: string;
-  district: string;
-  street: string;
-  streetNumber: string;
-  businessCircle: string;
-  adCode: string;
-  address: string;
-  description: string;
-} & LatLng;
-
-export class Geocode {
-  static search(address: string, city?: string): Promise<GeocodeSearchResult>;
-  static reverse(coordinate: LatLng): Promise<GeocodeReverseResult>;
-}
-
-export class Initializer {
-  static init(iosKey?: string): Promise<void>;
-}
-
-export type LocationOptions = {
-  gps: boolean;
-  distanceFilter: number;
-};
-
-type LocationListener = (
-  listener: {
-    timestamp: number;
-    altitude: number;
-    speed: number;
-  } & Location
-) => {};
-
-export class Geolocation {
-  static init(): Promise<void>;
-  static start(): Promise<void>;
-  static stop(): Promise<void>;
-  static setOptions(options: LocationOptions): Promise<void>;
-  static addLocationListener(listener: LocationListener): Promise<void>;
-}
+export default _default;
+export { MapView, Location, Geocode, Initializer };
---
+++ lib/js/types.d.ts
@@ -0,0 +1,31 @@
+export declare type Point = {
+    x: number;
+    y: number;
+};
+export declare type LatLng = {
+    latitude: number;
+    longitude: number;
+};
+export declare type Region = {
+    latitudeDelta: number;
+    longitudeDelta: number;
+} & LatLng;
+export declare type MapStatus = {
+    center: LatLng;
+    region: Region;
+    overlook: number;
+    rotation: number;
+    zoomLevel: number;
+};
+export declare type Location = {
+    accuracy?: number;
+    latitude: number;
+    longitude: number;
+    direction: number;
+};
+export declare type EdgePadding = {
+    top: Number;
+    right: Number;
+    bottom: Number;
+    left: Number;
+};
